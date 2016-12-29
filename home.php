e1dbb9c11eb55521a81d5408ae665acaa64e214e
Princeton <kountry.nelson@gmail.com>
2016-12-29 09:32:51 -0500

    committted

---
+++ home.php
@@ -0,0 +1,69 @@
+<html>
+<head>
+<title>Kountry Migo</title>
+<style>
+body { color: #3f312e; background: #ecd078; font: 14px Helvetica, Arial, Sans-serif;}
+#header {width: 960px; min-height: 150px; margin: 0 auto;}
+#navigation { width: 960px; margin: 0 auto;}
+#container { overflow: auto; width: 960px; margin: 0 auto;}
+#content { width: 960px; min-height: 400px; margin: 0 auto; background: #fff;}
+#contentleft {width: 580px; float: left; margin-right: 20px;}
+#sidebar {width: 280px; float: left;}
+#footer {width: 960px; margin: 0 auto;}
+</style>
+</head>
+<body>
+<div id="header">
+
+</div> <!--header-->
+<div id="navigation">
+<ul>
+     <li><a href="">Home</a></li>
+     <li><a href="">About</a></li>
+     <li><a href="">Contact</a></li>
+  </ul>   
+</div> <!--navigation-->
+
+<div id="container">
+<div id="content">
+<div id="contentleft">
+<h1> Welcome to my Page</h1>
+<p> Welcome to my site,thank you so much for visiting my personal website</p>
+<p> Your going to explore great music that i make perosnally, and i hope you are excited</p>
+<p> So jump right in to explore great music and relax to a blunt of Mary Jane </p>
+
+
+</div> <!--contentleft-->
+
+<div id="sidebar">
+<div class= "sidebar-element">
+<h3>About me </h3>
+<p> The music that i create comes from the heart, and I dedicate it to God </p>
+<p> everything comes from hard work, and i hope it pay off, im from the struggle </p>
+</div>
+
+<div class="sidebar-element">
+<h3> Unordered List </h3>
+<ul>
+	<li><a  href="">List Item #1</a></li>
+     <li><a href="">List Item #2</a></li>
+     <li><a href="">List Item #3</a></li>
+ </ul>    
+</div>
+
+</div> <!--sidebar-->
+</div> <!--content-->
+</div> <!--container-->
+
+<div id="footer">
+<ul>
+	<li><a href="">Home</a></li>
+     <li><a href="">About</a></li>
+     <li><a href="">Contact</a></li>
+ </ul> 
+ <p> &copy;2015 Kountrymigo.com</p>
+
+</div> <!--footer-->
+
+</body>
+</html>
